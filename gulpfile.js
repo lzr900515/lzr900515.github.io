a52aa03c2397c2800bea4c1a3e7d099c6a3f258f
lzr900515 <626966164@qq.com>
2017-04-24 16:58:27 +0800

    提交

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// 静态服务器
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// 代理
+
+//gulp.task('browser-sync', function() {
+//    browserSync.init({
+//        proxy: "http://localhost"
+//    });
+//});
