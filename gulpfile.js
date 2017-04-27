37965afe08487c056393dc1124da4ccee4cad8a1
lzr900515 <626966164@qq.com>
2017-04-27 10:48:35 +0800

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 // 静态服务器
-gulp.task('browser-sync', function() {
+gulp.task('start', function() {
     browserSync.init({
         server: {
             baseDir: "./"
